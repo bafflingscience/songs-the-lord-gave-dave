8ad63c870e33c96b07fe142be8e07525748d9bb3
Baffling Science <bafflingscience@protonmail.com>
2021-08-02 14:27:15 -0400

    babies gotta brand new branch

--- src/public/assets/js/dataSwitch.js
+++
@@ -1,9 +0,0 @@
-$('.album-poster').on('click', function (e) {
-  var dataSwitchId = $(this).attr('data-switch');
-  console.log(dataSwitchId);
-
-  ap.list.switch(dataSwitchId);
-
-  ap.play();
-  $('#aplayer').addClass('showPlayer')
-});  
